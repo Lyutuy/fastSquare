1dcaf9968f32171e824a3e765ca34ec55ca510bf
Lyutuy <lytuy03115@gmail.com>
2020-01-27 17:21:51 +0200

    add timer

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
+let $time = document.querySelector('#time');
+
 let score = 0;
+let isGameStarted = false;
 
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick);
 
 
 function startGame() {
+    isGameStarted = true;
     //console.log('Start');
     //hide 'Start' button
     $start.classList.add('hide');
     //change background color of frame
     $game.style.backgroundColor = '#fff';
+    let interval = setInterval(function() {
+        let time = parseFloat($time.textContent)
+
+        if (time <= 0) {
+            clearInterval(interval);
+            endGame();
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1);
+        }
+    }, 100);
+
 
     renderBox();
 }
 
+function endGame() {
+    isGameStarted = false;
+}
+
 function handleBoxClick(event) {
+    if (!isGameStarted) {
+        return
+    }
     if (event.target.dataset.box) {
         score++;
         renderBox();
@@ -30,7 +52,7 @@ function renderBox() {
     let boxSize = getRandom(10, 100);
     let fieldGameSize = $game.getBoundingClientRect();
     let maxTop = fieldGameSize.height - boxSize;
-    let maxLeft = fieldGameSize - boxSize;
+    let maxLeft = fieldGameSize.width - boxSize;
 
     box.style.height = box.style.width = boxSize + 'px';
     box.style.position = 'absolute';
